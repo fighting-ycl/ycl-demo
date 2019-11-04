48ee603cc9e44934e9b66b2768f6a5ff658ee54e
13513094569 <13513094569@163.com>
2019-11-04 18:35:05 +0800

    ok

--- builde.sh
+++ builde.sh
@@ -5,5 +5,5 @@ git pull
 version=`date +%Y%m%d%H%M%S`
 sudo mvn clean install -Dmaven.test.skip=true
 echo "Image Building..."
-sudo docker build -t registry.cn-beijing.aliyuncs.com/personal/ycl/spring-boot-demo:latest
+sudo docker build -t registry.cn-beijing.aliyuncs.com/personal/ycl/spring-boot-demo:latest .
 sudo docker push registry.cn-beijing.aliyuncs.com/personal/ycl/spring-boot-demo:latest
